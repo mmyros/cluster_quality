dc3a55e43d00f691b1fd6eed3f9a60ffc961da82
max <mmyros@gmail.com>
2020-06-29 23:25:29 -0400

    moving cluster_metrics into a separate repo

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cluster_quality',
+    version='0.0.1',
+    install_requires=[
+        'click',
+        'numpy',
+        'scipy',
+        'scikit-learn'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    py_modules=['cluster_quality'],
+    entry_points='''
+    [console_scripts]
+    cluster_quality=cluster_quality.scripts.cluster_quality:cli
+    ''',
+
+    url='',
+    license='MIT',
+    author='Maxym Myroshnychenko',
+    author_email='mmyros@gmail.com',
+    description='extras for spikesorting curation'
+)
