40fc9b0a627972f7a56b8d6b2ae9e839d43de9b8
m <mmyros@gmail.com>
2020-09-30 09:09:21 -0400

    another dataset for  tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -40,3 +40,11 @@ def test_load_kilosort_data():
     assert len(set(spike_clusters)) == 675
     assert sum(cluster_quality=='good') == 242
 
+    ### doublePhase3
+    base_path, files = download_test_data(base_url='http://data.cortexlab.net/doublePhase3/data/',
+                       base_path='tests/test_data/')
+    outputs = io.load_kilosort_data(base_path, include_pcs=True)
+    assert sum([output is None for output in outputs]) == 0, f'Missing some files? Some outputs are None: {outputs}'
+    (spike_times, spike_clusters, spike_templates, templates, amplitudes,
+     unwhitened_temps, channel_map, cluster_ids, cluster_quality, pc_features, pc_feature_ind) = outputs
+
